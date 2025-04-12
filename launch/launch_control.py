f365ef1a7f3a17571f278ed3fc64ab3acb88c370
jetson <jetson@wade.dev>
2025-04-12 06:32:46 -0400

    fix build

--- launch/launch_control.py
+++ launch/launch_control.py
@@ -8,8 +8,8 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='diffdrive_arduino',
-            executable='diffdrive_arduino',
-            name='diffdrive_arduino',
+            executable='diffdrive_arduino_node',
+            name='diffdrive_arduino_node',
             parameters=[os.path.join(
                 get_package_share_directory("diffdrive_arduino"),'config','params.yaml'
             )],
